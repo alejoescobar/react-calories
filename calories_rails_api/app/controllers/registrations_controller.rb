1c9386ec2ac0153b0c7745f0bd97942156e01716
alejoescobar <alejoescobac@gmail.com>
2016-11-27 14:35:13 -0500

    Add registrations controller and create action

---
+++ calories_rails_api/app/controllers/registrations_controller.rb
@@ -0,0 +1,18 @@
+class RegistrationsController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 400
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :daily_calories_goal)
+    end
+    
+end
