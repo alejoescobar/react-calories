d80fd352f41c0025936f0a316214ed5895a0b292
alejoescobar <alejoescobac@gmail.com>
2016-11-27 14:48:48 -0500

    Setup react-router

--- calories_react_client/src/index.js
+++ calories_react_client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Registration from './Registration'
+import Login from './Login'
+import { Router, Route, browserHistory } from 'react-router'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/registration" component={Registration} />
+    <Route path="/login" component={Login} />
+  </Router>,
   document.getElementById('root')
 );
