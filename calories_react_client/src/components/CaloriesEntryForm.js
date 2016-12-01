067ba3c4108ee2d35e6c9b81faa148502938ed7b
alejoescobar <alejoescobac@gmail.com>
2016-12-01 18:23:29 -0500

    Add calories entry from and use it on new calories entry

---
+++ calories_react_client/src/components/CaloriesEntryForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import AlertDismissable from '../components/AlertDismissable'
+import FieldGroup from '../components/FieldGroup'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router'
+
+class CaloriesEntryForm extends Component {
+  render() {
+    const alerts = this.props.errors.map((message, index) =>
+      <AlertDismissable key={index} message={message}/>
+    )
+    return (
+      <div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <Link to="/calories">Back</Link>
+          <h1>New Calories Entry</h1>
+          <hr/>
+          {alerts}
+          <form onSubmit={this.props.onSubmitForm}>
+            <FieldGroup
+              id="title"
+              type="text"
+              label="Title:"
+              placeholder="Enter the title of the new entry"
+              required="true"
+              value={this.props.title}
+              onChange={this.props.onUpdateTitle}/>
+            <FieldGroup
+              id="caloriesAmount"
+              type="number"
+              label="Calories Amount:"
+              placeholder="Enter the amount of calories the new entry"
+              required="true"
+              value={this.props.caloriesAmount}
+              onChange={this.props.onUpdateCaloriesAmount}/>
+            <FieldGroup
+              id="caloriesAmount"
+              type="datetime-local"
+              label="Date (mm/dd/yyyy):"
+              placeholder="Enter the amount of calories the new entry"
+              required="true"
+              value={this.props.date}
+              onChange={this.props.onUpdateDate}/>
+            <Button bsStyle="success" type="submit">
+              Create new entry
+            </Button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CaloriesEntryForm
--- calories_react_client/src/views/NewCaloriesEntry.js
+++ calories_react_client/src/views/NewCaloriesEntry.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
-import FieldGroup from '../components/FieldGroup'
-import { Button } from 'react-bootstrap'
 import requests from '../requestsHelper'
 import { browserHistory } from 'react-router'
-import { Link } from 'react-router'
-import AlertDismissable from '../components/AlertDismissable'
+import CaloriesEntryForm from '../components/CaloriesEntryForm'
 
 class NewCaloriesEntry extends Component {
   constructor(props) {
@@ -43,53 +40,21 @@ class NewCaloriesEntry extends Component {
       browserHistory.push('/calories')
     })
     .catch((error) => {
-      console.log(error);
       this.setState({ errors: error.response.data.errors })
     })
   }
 
   render() {
-    const alerts = this.state.errors.map((message, index) =>
-      <AlertDismissable key={index} message={message}/>
-    )
     return (
-      <div>
-        <div className="col-sm-8 col-sm-offset-2">
-          <Link to="/calories">Back</Link>
-          <h1>New Calories Entry</h1>
-          <hr/>
-          {alerts}
-          <form onSubmit={this.handleSubmit}>
-            <FieldGroup
-              id="title"
-              type="text"
-              label="Title:"
-              placeholder="Enter the title of the new entry"
-              required="true"
-              value={this.state.title}
-              onChange={this.updateTitle}/>
-            <FieldGroup
-              id="caloriesAmount"
-              type="number"
-              label="Calories Amount:"
-              placeholder="Enter the amount of calories the new entry"
-              required="true"
-              value={this.state.caloriesAmount}
-              onChange={this.updateCaloriesAmount}/>
-            <FieldGroup
-              id="caloriesAmount"
-              type="datetime-local"
-              label="Date (mm/dd/yyyy):"
-              placeholder="Enter the amount of calories the new entry"
-              required="true"
-              value={this.state.date}
-              onChange={this.updateDate}/>
-            <Button bsStyle="success" type="submit">
-              Create new entry
-            </Button>
-          </form>
-        </div>
-      </div>
+      <CaloriesEntryForm
+        onSubmitForm={this.handleSubmit}
+        onUpdateTitle={this.updateTitle}
+        onUpdateCaloriesAmount={this.updateCaloriesAmount}
+        onUpdateDate={this.updateDate}
+        title={this.state.title}
+        caloriesAmount={this.state.caloriesAmount}
+        date={this.state.date}
+        errors={this.state.errors}/>
     )
   }
 }
