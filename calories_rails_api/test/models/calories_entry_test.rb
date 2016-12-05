8001e32d496bfb4faeb3abbca942af6e7532decf
alejoescobar <alejoescobac@gmail.com>
2016-12-05 00:14:41 -0500

    Add shoulda matchers and test models

--- calories_rails_api/test/models/calories_entry_test.rb
+++ calories_rails_api/test/models/calories_entry_test.rb
@@ -14,7 +14,47 @@
 require 'test_helper'
 
 class CaloriesEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to :user
+  should validate_presence_of :date
+  should validate_presence_of :title
+  should validate_presence_of :calories_amount
+
+  test 'returns date field formatted as a date string' do
+    calories_entry = calories_entries(:one)
+    day = calories_entry.day
+    assert_equal day, calories_entry.date.strftime('%a, %-e %b %Y')
+  end
+
+  test 'returns day field formatted as a time string' do
+    calories_entry = calories_entries(:one)
+    time = calories_entry.time
+    assert_equal time, calories_entry.date.strftime('%-l:%M %p')
+  end
+
+  test 'group_by_day scope returns calories entries grouped by day' do
+    calories_entries = CaloriesEntry.group_by_day
+    total_entries = 0
+    calories_entries.each { |obj| total_entries += obj[:entries].length }
+
+    assert_equal CaloriesEntry.count, total_entries
+  end
+
+  test 'filter_by_date scope return entries between two dates' do
+    calories_entry = calories_entries(:one)
+    start_date = calories_entry.date - 1.day
+    end_date = calories_entry.date + 1.day
+    calories_entries = CaloriesEntry.filter_by_date(start_date, end_date)
+
+    assert_includes calories_entries, calories_entry
+  end
+
+  test 'filter_by_time scope return entries between two times' do
+    calories_entry = calories_entries(:one)
+    start_time = calories_entry.date - 1.hour
+    end_time = calories_entry.date + 1.hour
+    calories_entries = CaloriesEntry.filter_by_time(start_time, end_time)
+
+    assert_includes calories_entries, calories_entry
+  end
+
 end
--- calories_rails_api/test/models/user_test.rb
+++ calories_rails_api/test/models/user_test.rb
@@ -15,7 +15,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should have_many(:calories_entries).dependent(:destroy)
+  should have_secure_password
+  should validate_presence_of :email
+  should validate_presence_of :daily_calories_goal
+  should validate_uniqueness_of :email
+
+  test 'authenticate should generate new auth_token' do
+    user = users(:user)
+    previous_token = user.auth_token
+    user.update(password: '12345678')
+    user.authenticate(user.password)
+    assert_not_equal previous_token, user.reload.auth_token
+  end
+
+  test 'sign out should generate new auth_token' do
+    user = users(:user)
+    previous_token = user.auth_token
+    user.sign_out
+    assert_not_equal previous_token, user.reload.auth_token
+  end
 end
